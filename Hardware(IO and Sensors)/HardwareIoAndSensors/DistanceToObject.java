f870b840a1969bbd53f62048e433434a59724045
skaushik008 <skaushik@stu.naperville203.org>
2018-05-22 11:20:46 -0500

    HardwareIo

--- Hardware(IO and Sensors)/HardwareIoAndSensors/DistanceToObject.java
+++ Hardware(IO and Sensors)/HardwareIoAndSensors/DistanceToObject.java
@@ -2,32 +2,10 @@
 /**
  * Write a description of class DistanceToObject here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author CoolMathGamers
+ * @version 5/22/18
  */
 public class DistanceToObject
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class DistanceToObject
-     */
-    public DistanceToObject()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+   
 }
